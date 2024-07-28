ae4f85c61851b3c79fb5cfc4c8c3767a635dfa25
valerie smith <valerie.plante.11@gmail.com>
2024-07-28 15:24:59 -0400

    changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-const isDevEnv = false
+
 
 export default defineConfig({
   plugins: [react()],
@@ -14,12 +14,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/students': {
-        target: isDevEnv ? 'http://127.0.0.1:9000' : 'https://globalbackend-zued.onrender.com',
+        target: 'https://globalbackend-zued.onrender.com',
         changeOrigin: true,
         secure: false,
       },
       '/courses': {
-        target: isDevEnv ? 'http://127.0.0.1:9000' : 'https://globalbackend-zued.onrender.com',
+        target: 'https://globalbackend-zued.onrender.com',
         changeOrigin: true,
         secure: false,
       },
